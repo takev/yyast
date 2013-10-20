ab4b37be8c2d19e65dcba01359c427b4fdca8731
Take Vos <take.vos@vosgames.nl>
2013-10-20 23:33:33 +0200

    Create a generic parser for yyast files.

--- pyext/parser.py
+++ pyext/parser.py
@@ -1,37 +1,150 @@
 
-import .yyast
+import yyast
 import mmap
 import struct
 
-def parse_node(m):
-    (
-        node_name,
-        node_size,
-        line_nr,
-        column_nr,
-        file_nr,
-        reserved1,
-        reserved2,
-        node_type
-    ) = struct.unpack(">8sQLLLHBB", m[:32])
+class YYASTParserException (Exception):
+    def __init__(self, message):
+        Exeption.__init__(self, args=(message,))
 
-    if line_nr   == 0xffffffff: line_nr   = None
-    if column_nr == 0xffffffff: column_nr = None
-    if file_nr   == 0xffffffff: file_nr   = None
+class Parser (object):
+    def __init__(self):
+        self.classes = {}
+        self.subparsers = {
+            yyast.NODE_TYPE_NULL:               self.parse_null_node,
+            yyast.NODE_TYPE_LEAF:               self.parse_leaf_node,
+            yyast.NODE_TYPE_BRANCH:             self.parse_branch_node,
+            yyast.NODE_TYPE_TEXT:               self.parse_text_node,
+            yyast.NODE_TYPE_POSITIVE_INTEGER:   self.parse_positive_integer_node,
+            yyast.NODE_TYPE_NEGATIVE_INTEGER:   self.parse_negative_integer_node,
+            yyast.NODE_TYPE_BINARY_FLOAT:       self.parse_binary_float_node,
+            yyast.NODE_TYPE_DECIMAL_FLOAT:      self.parse_decimal_float_node,
+            yyast.NODE_TYPE_LIST:               self.parse_list_node,
+            yyast.NODE_TYPE_COUNT:              self.parse_count_node,
+        }
 
-    node_info = {
-        "node_name": node_name,
-        "node_size": node_size,
-        "line_nr": line_nr,
-        "column_nr": column_nr,
-        "file_nr": file_nr,
-        "node_type": node_type,
-    }
+    def register_factory(self, node_name, factory):
+        """Register a class for parsing a node.
 
-    base_class = base_classes[node_type]
-    class_name = "Node_" + ("__at_" + node_name[1:] if node_name[0] == "@" else node_name)
+        @param node_name    The name of the node in the file to be parsed.
+        @param factory      The class that should be used as a factory for this node.
+        """
+        self.factories[node_name] = factory
 
-def parse(fd):
-    m = mmap.mmap(fd.fileno(), 0)
-    return parse_node(m)
+    def parse_null_node(self, factory, node_info, inner_data):
+        if len(inner_data) > 0:
+            raise YYASTParserException("Null node should not have data.")
+
+        return factory(node_info)
+
+    def parse_leaf_node(self, factory, node_info, inner_data):
+        if len(inner_data) > 0:
+            raise YYASTParserException("Leaf node should not have data.")
+
+        return factory(node_info)
+
+    def parse_branch_node(self, factory, node_info, inner_data):
+        node = factory(node_info)
+
+        # Parse the child nodes and add it to the node.
+        while internal_data:
+            child_node, internal_data = self.parse_node(internal_data)
+            node.add_child(child_node)
+        
+        return node
+
+    def parse_text_node(self, factory, node_info, inner_data):
+        value = inner_data.decode("UTF-8")
+        return factory(node_info, value)
+
+    def parse_positive_integer_node(self, factory, node_info, inner_data):
+        if len(inner_data) != 8:
+            raise YYASTParserException("Positive integer node should be exactly 64 bit.")
+
+        (value,) = struct.unpack(">Q", inner_data)
+        return factory(node_info, value)
+
+    def parse_negative_integer_node(self, factory, node_info, inner_data):
+        if len(inner_data) != 8:
+            raise YYASTParserException("Negative integer node should be exactly 64 bit.")
+
+        (value,) = struct.unpack(">Q", inner_data)
+        return factory(node_info, -value)
+
+    def parse_binary_float_node(self, factory, node_info, inner_data):
+        if len(inner_data) != 8:
+            raise YYASTParserException("Binary float node should be exactly 64 bit.")
+
+        (value,) = struct.unpack(">d", inner_data)
+        return factory(node_info, value)
+
+    def parse_decimal_float_node(self, factory, node_info, inner_data):
+        if len(inner_data) != 8:
+            raise YYASTParserException("Decimal float node should not have data.")
+
+        raise NotImplementedError("Decimal float is not implemented in the parser.")
+
+    def parse_list_node(self, factory, node_info, inner_data):
+        raise NotImplementedError("List node should not exist in the file.")
+
+    def parse_count_node(self, factory, node_info, inner_data):
+        raise NotImplementedError("Count node should not exist in the file.")
+
+    def parse_node(self, data):
+        """Parse a single node from the data
+
+        This function will parse the data and recursively create and return nodes.
+
+        @param data Data from a yyast file.
+        @return node, rest  The parsed node, the rest data that is left after parsing the node.
+        """
+
+        # Parse the header of the data.
+        (
+            node_name,
+            node_size,
+            line_nr,
+            column_nr,
+            file_nr,
+            reserved1,
+            reserved2,
+            node_type
+        ) = struct.unpack(">8sQLLLHBB", data[:32])
+
+        # The internal data for the node starts from the header, until the length of the node data.
+        internal_data = data[32:node_size]
+
+        # The rest of the data after the node.
+        rest_data = data[node_size:]
+
+        # Convert fields into more usable values.
+        if line_nr   == 0xffffffff: line_nr   = None
+        if column_nr == 0xffffffff: column_nr = None
+        if file_nr   == 0xffffffff: file_nr   = None
+        node_name = node_name.strip()
+
+        node_info = {
+            "node_name":    node_name,
+            "node_size":    node_size,
+            "line_nr":      line_nr,
+            "column_nr":    column_nr,
+            "file_nr":      file_nr,
+            "node_type":    node_type,
+        }
+
+        # Get the factory class for the node.
+        factory = self.factories[node_name]
+
+        try:
+            subparser = self.subparsers[node_type]
+        except KeyError:
+            raise YYASTParserException("Unknown node type %i.", node_type)
+        
+        node = subparser(factory, node_info, inner_data)
+        return node, rest_data
+
+
+    def parse(self, fd):
+        m = mmap.mmap(fd.fileno(), 0)
+        return parse_node(m)
 
--- pyext/yyast.py
+++ pyext/yyast.py
@@ -1,3 +1,26 @@
+# Copyright (c) 2011-2013, Take Vos
+# All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
+#
+# - Redistributions of source code must retain the above copyright notice,
+#   this list of conditions and the following disclaimer.
+# - Redistributions in binary form must reproduce the above copyright notice, 
+#   this list of conditions and the following disclaimer in the documentation 
+#   and/or other materials provided with the distribution.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" 
+# AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE 
+# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE 
+# ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE 
+# LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR 
+# CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+# SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS 
+# INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN 
+# CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+# ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE 
+# POSSIBILITY OF SUCH DAMAGE.
 
 
 NODE_TYPE_NULL              = 0
@@ -8,3 +31,7 @@ NODE_TYPE_POSITIVE_INTEGER  = 4
 NODE_TYPE_NEGATIVE_INTEGER  = 5
 NODE_TYPE_BINARY_FLOAT      = 6
 NODE_TYPE_DECIMAL_FLOAT     = 7
+NODE_TYPE_LIST              = 254   # Never encoded in stream.
+NODE_TYPE_COUNT             = 255   # Never encoded in stream.
+
+
